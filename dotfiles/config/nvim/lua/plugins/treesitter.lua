5902398d59a3de0d10f097db4084c0f14dc32935
Jim Gaylard <jim@jimgaylard.com>
2026-06-16 17:06:05 +1000

    nvim: use opts and main for treesitter configuration

--- dotfiles/config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/config/nvim/lua/plugins/treesitter.lua
@@ -2,17 +2,16 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "javascript", "markdown", "html", "css", "dockerfile", "bash", "lua", "go", "python", "yaml" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = { "javascript", "markdown", "html", "css", "dockerfile", "bash", "lua", "go", "python", "yaml" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    },
   }
 }
